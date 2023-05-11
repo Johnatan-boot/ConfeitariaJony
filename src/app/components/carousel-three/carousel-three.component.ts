ab1f7c0ac14bf96e2ebf8ab0a2f66688ec30174a
johnatan quenes dos santos <johnatan.quenes@gmail.com>
2023-05-11 20:35:46 -0300

    desenvolvido Sitema ecommerce confeitaria

---
+++ src/app/components/carousel-three/carousel-three.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-three',
+  templateUrl: './carousel-three.component.html',
+  styleUrls: ['./carousel-three.component.css']
+})
+export class CarouselThreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/pages/cadastro-u/cadastro-u.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-u',
+  templateUrl: './cadastro-u.component.html',
+  styleUrls: ['./cadastro-u.component.css']
+})
+export class CadastroUComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
